50a755aac8fe0fb1ba786c04fddb48e4cb727d06
imguuh21 <im.guuh21@gmail.com>
2026-02-14 13:01:40 +0000

    Add files via upload

--- MultiQuiz.py
+++ MultiQuiz.py
@@ -1,12 +1,10 @@
 import MultiQuiz
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, NumericProperty, ListProperty,OptionProperty, ObjectProperty
-from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.recycleview import RecycleView
+
 
 Quizes = [
     {"Título": "Quiz 1","Descrição": "Quiz sobre Python", "detalhes": "Detalhes completos da notícia 1..."},
@@ -14,6 +12,17 @@ Quizes = [
     {"Título": "Quiz 3","Descrição": "Quiz sobre Java", "detalhes": "Detalhes completos da notícia 3..."},
     {"Título": "Quiz 4","Descrição": "Quiz sobre Ethical hacking", "detalhes": "Detalhes completos da notícia 4..."}
 ]
+
+pergunta_quiz_1 = ({{
+    "pergunta":"Quem foi o Criador do Python?",
+    "respostas":["Bill Gates","Elon Musk","Guido Van Rossum"],
+    "correta": "Guido Van Rossum"
+},{
+    "pergunta":"Qual ano Guido Van Rossum criou Python?",
+    "respostas":""
+}
+})
+
 class TelaPrincipal(Screen):
     lista = ObjectProperty(None)
 
@@ -22,32 +31,31 @@ class TelaPrincipal(Screen):
 
 
     def carregar(self):
-        self.lista.data = []
-        for item in Quizes:
-            Quiz = {
-                "text": f"{item["Título"]}\n{item['Descrição']}",
+        self.ids.lista_quizes.data = [
+            {
+                "text": f"{item['Título']}\n{item['Descrição']}",
                 "on_release": lambda x=item: self.abrir_detalhes(x)
-            }
-            self.lista.data.append(Quiz)
+            } for item in Quizes
+        ]
 
-            def abrir_detalhes(self, noticia):
-                tela_detalhes = self.manager.get_screen('detalhes')
-                tela_detalhes.atualizar_conteudo(noticia)
-                self.manager.current = 'detalhes'
+    def abrir_detalhes(self, item):
+          tela_detalhes = self.manager.get_screen('detalhes')
+          tela_detalhes.atualizar_conteudo
+          self.manager_current = 'detalhes'
 
 class TelaDetalhes(Screen):
-    titulo = ObjectProperty(None)
-    descricao = ObjectProperty(None)
+    def atualizar_conteudo(self, item):
+            self.ids.text = item['título']
+            self.ids.text = item['detalhes']
 
-    def atualizar(self, noticia):
-            self.titulo.text = noticia['título']
-            self.descrição.text = noticia['detalhes']
+class ListaQuizes(RecycleView):
+    pass
 
-class ListaQuizes(RecycleView): pass
+class Gerenciador(ScreenManager):
+    pass
 
-class Gerenciador(ScreenManager): pass
-
-class NoticiasApp(App):
-    def build(self): return Gerenciador()
-
-MultiQuiz.App().run()
+class Multiquiz(App):
+    def build(self):
+        return Gerenciador()
+if __name__ == "__main__":
+    MultiQuiz.App().run()
---
+++ todo.md
@@ -0,0 +1,7 @@
+1-criar interface do multiquiz
+
+2-preparar a perguntas
+
+3-RecycleView MultiScreenManager
+
+4-backend/ficheiro.kv
